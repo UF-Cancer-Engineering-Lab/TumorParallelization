2e282ab7c83d1407cbda5a689be904bab1a867c9
haroonsyed <haroonaminsyed@gmail.com>
2023-05-10 05:21:14 -0400

    Example with device functions

--- cuda_kernels/kernel_loader.py
+++ cuda_kernels/kernel_loader.py
@@ -10,13 +10,13 @@ def get_cuda_functions():
             file = open("./cuda_kernels/" + filename)
             file_code = file.read()
             func_name = filename.removesuffix(".cu")
-            cuda_functions[func_name] = cp.RawKernel(file_code, 'vector_add')
+            cuda_functions[func_name] = cp.RawKernel(file_code, func_name)
     return cuda_functions
 
-# cuda_kernels = get_cuda_functions()
+cuda_kernels = get_cuda_functions()
 
-# x1 = cp.arange(25, dtype=cp.float32).reshape(5, 5)
-# x2 = cp.arange(25, dtype=cp.float32).reshape(5, 5)
-# y = cp.zeros((5, 5), dtype=cp.float32)
-# cuda_kernels["vector_add"]((5,), (5,), (x1, x2, y))
-# print(y)
+x1 = cp.arange(25, dtype=cp.float32).reshape(5, 5)
+x2 = cp.arange(25, dtype=cp.float32).reshape(5, 5)
+y = cp.zeros((5, 5), dtype=cp.float32)
+cuda_kernels["vector_add"]((5,), (5,), (x1, x2, y))
+print(y)
--- cuda_kernels/vector_add.cu
+++ cuda_kernels/vector_add.cu
@@ -1,5 +1,11 @@
+extern "C" __device__ void print_hi()
+{   
+    printf("HI!");
+}
+
 extern "C" __global__ void vector_add(float *a, float *b, float *c)
 {   
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
     c[tid] = a[tid] + b[tid];
+    print_hi();
 }
