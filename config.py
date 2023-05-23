ea42b72c2b2bbeade355f0416f1d5837ba295533
haroonsyed <haroonaminsyed@gmail.com>
2023-05-23 03:38:31 -0400

    Move config to json file

--- config.py
+++ config.py
@@ -1,14 +1,19 @@
+import json
+config_path = "./config/config.json"
+config_data = json.load(open(config_path))
+
+
 # ----------------------------------------- Program Parameters --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # defining parameters of the simulation
-n = 100  # number of timeSteps
-maxTries = 6  # max tries for a particle to move
-particlesNumber = 1  # initial particle count
-porosityFraction = 0.05  # porosity fraction of particles,
+n = config_data["n"]  # number of timeSteps
+maxTries = config_data["maxTries"]  # max tries for a particle to move
+particlesNumber = config_data["particlesNumber"]  # initial particle count
+porosityFraction = config_data["porosityFraction"]  # porosity fraction of particles,
 # where porosity fraction is the ratio of void volume to total volume
 # each "particle", or "cell" has some void space in it
-sphereRadius = 12
-shouldSaveResults = False
-show3DVisualization = True
-outPath = "./output2/"
-scene_file_name = "test_scene"
-max_vram_allocation_gb = 1
+sphereRadius = config_data["sphereRadius"]
+shouldSaveResults = config_data["shouldSaveResults"]
+show3DVisualization = config_data["show3DVisualization"]
+outPath = config_data["outPath"]
+scene_file_name = config_data["scene_file_name"]
+max_vram_allocation_gb = config_data["max_vram_allocation_gb"]
